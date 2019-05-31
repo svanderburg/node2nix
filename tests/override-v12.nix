bb4d77b2fd4b5624e3da7e7c11602d8697678bb4
Serhii Khoma <srghma@gmail.com>
2019-05-31 15:12:04 +0300

    fix: nix-build ./release.nix -> no file tests/override-v12.nix error

---
+++ tests/override-v12.nix
@@ -0,0 +1,14 @@
+{pkgs ? import <nixpkgs> {
+    inherit system;
+}, system ? builtins.currentSystem}:
+
+let
+  nodePackages = import ./default-v12.nix {
+    inherit pkgs system;
+  };
+in
+nodePackages // {
+  floomatic = nodePackages.floomatic.override {
+    buildInputs = [ pkgs.pkgconfig pkgs.qt4 ];
+  };
+}
