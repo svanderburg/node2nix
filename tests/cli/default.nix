b82e930a46090002cc2c5d996e0e39cf5182cb71
Sander van der Burg <svanderburg@gmail.com>
2020-12-02 20:26:25 +0100

    Migrate to the Python version of the NixOS test driver

--- tests/cli/default.nix
+++ tests/cli/default.nix
@@ -3,7 +3,7 @@
 let
   testprojects = ./testprojects;
 in
-with import "${nixpkgs}/nixos/lib/testing.nix" { system = builtins.currentSystem; };
+with import "${nixpkgs}/nixos/lib/testing-python.nix" { system = builtins.currentSystem; };
 
 simpleTest {
   nodes = {
@@ -15,35 +15,35 @@ simpleTest {
   };
   testScript =
     ''
-      startAll;
+      start_all()
 
-      $machine->mustSucceed("cp -av ${testprojects} testprojects");
-      $machine->mustSucceed("chmod u+w testprojects");
+      machine.succeed(
+          "cp -av ${testprojects} testprojects"
+      )
+      machine.succeed("chmod u+w testprojects")
 
       # Try to generate Nix expressions for a package.json with no name. This should fail.
-      $machine->mustFail("cd testprojects/noname; node2nix");
+      machine.fail("cd testprojects/noname; node2nix")
 
       # Try to generate Nix expressions for a package.json with no version. This should fail.
-      $machine->mustFail("cd testprojects/noversion; node2nix");
+      machine.fail("cd testprojects/noversion; node2nix")
 
       # Try to generate Nix expressions for a package.json with a name and version. This should succeed.
-      $machine->mustSucceed("cd testprojects/complete; node2nix");
+      machine.succeed("cd testprojects/complete; node2nix")
 
       # Try to generate Nix expression from package.json without specifying the lock file. This should raise a warning.
-      my $output = $machine->mustSucceed("cd testprojects/lock; node2nix -8");
-      if($output =~ /lock file exists/) {
-          print "Lock file warning raised\n";
-      } else {
-          die "A lock file warning should be raised";
-      }
+      output = machine.succeed("cd testprojects/lock; node2nix -8")
+      if "lock file exists" in output:
+          print("Lock file warning raised")
+      else:
+          raise Exception("A lock file warning should be raised")
 
       # Try to generate Nix expression in a project that has a node_modules/ sub folder. This should raise a warning.
-      $output = $machine->mustSucceed("cd testprojects/garbage; node2nix");
+      output = machine.succeed("cd testprojects/garbage; node2nix")
 
-      if($output =~ /node_modules\/ folder in the root directory/) {
-          print "node_modules/ warning raised\n";
-      } else {
-          die "A node_modules/ warning should be raised";
-      }
+      if "node_modules/ folder in the root directory":
+          print("node_modules/ warning raised")
+      else:
+          raise Exception("A node_modules/ warning should be raised")
     '';
 }
